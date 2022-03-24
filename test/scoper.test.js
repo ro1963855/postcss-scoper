1c17ca2df67c6500d3ae6946ecdc6c1cfceb45a0
Dandy Chen <ro1963855@gmail.com>
2022-03-24 18:21:13 +0800

    change to postcss v7 api

--- test/scoper.test.js
+++ test/scoper.test.js
@@ -10,37 +10,41 @@ function read (name) {
 
 describe('scoper', () => {
   test('should throw when passing undefined scope', () => {
-    expect(() => postcss()
+    const results = postcss()
       .use(scoper())
       .process('.a {}')
-    ).toThrow(TypeError)
+    expect(() => results.css).toThrow()
   })
 
   test('should throw when passing invalid scope type', () => {
-    expect(() => postcss([scoper({
-      scope: 0,
-    })]).process('.a {}')).toThrow(TypeError)
+    const results = postcss()
+      .use(scoper({ scope: 0 }))
+      .process('.a {}')
+    expect(() => results.css).toThrow()
   })
 
   test('should throw when passing invalid scope selector', () => {
-    expect(() => postcss([scoper({
-      scope: 'a >> b',
-    })]).process('.a {}')).toThrow(TypeError)
+    const results = postcss()
+      .use(scoper({ scope: 'a >> b' }))
+      .process('.a {}')
+    expect(() => results.css).toThrow()
   })
 
   test('should scope all selector css', () => {
-    const { css } = postcss([scoper({
-      scope: '.scope',
-    })]).process(read('selector'))
+    const { css } = postcss()
+      .use(scoper({ scope: '.scope' }))
+      .process(read('selector'))
 
     expect(css).toEqual(read('selector.out'))
   })
 
   test('should overwrite selector in the overwrite option', () => {
-    const { css } = postcss([scoper({
-      scope: '.scope',
-      overwrites: ['html', 'body'],
-    })]).process(read('overwrite'))
+    const { css } = postcss()
+      .use(scoper({
+        scope: '.scope',
+        overwrites: ['html', 'body'],
+      }))
+      .process(read('overwrite'))
 
     expect(css).toEqual(read('overwrite.out'))
   })
