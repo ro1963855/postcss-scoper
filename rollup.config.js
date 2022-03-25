8d0bb2fe5bd8e8586fd49fff94203b70ee98382b
Dandy Chen <ro1963855@gmail.com>
2022-03-25 16:04:42 +0800

    all files use import/export

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,28 @@
 import { uglify } from 'rollup-plugin-uglify'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
 
 export default {
   input: 'lib/scoper.js',
   output: {
     file: 'dist/bundle.js',
     format: 'cjs',
+    exports: 'default',
   },
   plugins: [
     uglify({
       mangle: {
         toplevel: true,
       },
+    }),
+    resolve(),
+    commonjs({
+      include: ['node_modules/**'],
+      dynamicRequireTargets: [
+        'node_modules/postcss/lib/css-syntax-error.js',
+        'node_modules/postcss/lib/parse.js',
+        'node_modules/postcss/lib/container.js',
+      ],
     }),
   ],
 }
