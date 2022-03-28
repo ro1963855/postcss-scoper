b9b68587fbd023acdd7394999f3a697f33520e48
Dandy Chen <ro1963855@gmail.com>
2022-03-28 17:12:40 +0800

    try codecov

--- jest.config.js
+++ jest.config.js
@@ -5,4 +5,6 @@ module.exports = {
   testEnvironment: 'jsdom',
   testRegex: '/test/.*\\.test?\\.ts$',
   moduleFileExtensions: ['ts', 'js'],
+  collectCoverage: true,
+  collectCoverageFrom: ['./lib/**'],
 }
